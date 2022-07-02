23d128b4b0b10ca3b60ff86cf075449ec0aef5e6
druva25894 <druva25894@gmail.com>
2022-07-02 17:05:11 +0530

    first commit

---
+++ src/main/scala/scala_practice.scala
@@ -0,0 +1,14 @@
+object scala_practice {
+
+  def main(args: Array[String]): Unit = {
+    var p = 30
+    var q = 20
+    var r = 10
+    var sum = p+q
+    println("sum is "+sum)
+
+  }
+
+
+
+}
---
+++ src/main/scala/scala_practice1.scala
@@ -0,0 +1,14 @@
+class GFG {
+  var name =
+
+  def show(): Unit =
+  {
+    println("hello I am " + name + " my age is " +age, "I am from "+branch)
+  }
+}
+object main {
+  def main(args: Array[String]): Unit = {
+    var ob = new GFG();
+    ob.show();
+  }
+}
